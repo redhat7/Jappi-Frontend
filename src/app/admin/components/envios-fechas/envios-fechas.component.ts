951dd4a1ca0b3ee242d168fb02aa6ab3663400e0
Maycon Guzman <maycon.guzman@sura.pe>
2023-05-28 02:09:07 -0500

    Feat(Envios-fechas): Se arregla filtro de busqueda

--- src/app/admin/components/envios-fechas/envios-fechas.component.ts
+++ src/app/admin/components/envios-fechas/envios-fechas.component.ts
@@ -17,8 +17,9 @@ import { environment } from './../../../../environments/environment';
 })
 export class EnviosFechasComponent implements OnInit {
   token: string;
+  currentDate: any;
 
-  data: [];
+  data: any[] = [];
   datos: [];
   id: any;
   distritos: Distrito[];
@@ -30,17 +31,6 @@ export class EnviosFechasComponent implements OnInit {
   @Output() searchClick: EventEmitter<void> = new EventEmitter<void>();
 
   @Input() valorSpinner: string;
-  @Input() spinner = [
-    { descripcion: "--- Elige un campo ---", id: "" },
-    { descripcion: "Descripcion del producto", id: "descripcion" },
-    { descripcion: "Distrito", id: "distritoName" },
-    { descripcion: "Estado", id: "nombre_estado" },
-    { descripcion: "Modo de entrega", id: "modo_entrega" },
-    { descripcion: "Nombre de la empresa", id: "nombre" },
-    { descripcion: "Nombre del Comprador", id: "nombres" },
-    { descripcion: "Nombre Motorizado", id: "nombre_motorizado" },
-    { descripcion: "Telefono del Comprador", id: "tele_entrega" },
-  ];
   @Input() searchTxt: string;
 
   constructor(
@@ -54,183 +44,181 @@ export class EnviosFechasComponent implements OnInit {
 
   ngOnInit(): void {
     const currentUser = JSON.parse(localStorage.getItem("auth"));
-    const currentDate = this.convertNewDateToString();
     this.token = currentUser.token;
+    this.currentDate = this.getCurrentDate();
     this.distritos = this.formService.getDistritos();
     this.zonas = this.formService.getZonas();
     this.estadoEntrega = this.formService.getEstadoEntrega();
-    this.getDataByDate(this.token, currentDate, currentDate);
+    this.getDataByDate(this.token, this.currentDate, this.currentDate);
   }
 
-  selectSpinner(value: string) {
-    this.valorSpinner = value;
-    console.log(this.valorSpinner);
+  getCurrentDate() {
+    const currentDate = this.formatApíDate('date', new Date());
+    return currentDate;
   }
 
-  editarEnvio(envio: any) {
-    console.log(envio);
-    const param = {
-      envio,
-      token: this.token,
-      distritos: this.distritos,
-      zonas: this.zonas,
-    };
-    this.ModalEditEnvios(param);
+  formatApíDate(type: 'date' | 'string', value: any): string {
+    let day: string;
+    let month: string;
+    let year: string;
+
+    if (type === 'date') {
+      const dateValue = new Date(value);
+      day = ('0' + dateValue.getDate()).slice(-2);
+      month = ('0' + (dateValue.getMonth() + 1)).slice(-2);
+      year = String(dateValue.getFullYear());
+    }
+
+    if (type === 'string') {
+      const [yearStr, monthStr, dayStr] = value.split('-');
+      day = dayStr;
+      month = monthStr;
+      year = yearStr;
+    }
+
+    return `${day}-${month}-${year}`;
   }
 
-  ModalEditEnvios({ envio, token, distritos, zonas }) {
-    const validate = 0;
-    const modalRef = this.dialog.open(ModalEnviosFechaComponent, {
-      data: {
-        envio,
-        token,
-        validate,
-        distritos,
-        zonas,
-      },
-      minWidth: "400px",
-      maxWidth: "800px",
-    });
-    modalRef.afterClosed().subscribe((result) => {
-      console.log("cerrando modal");
-      console.log(result);
-      const currentDate = this.convertNewDateToString();
-      result
-        ? this.getDataByDate(this.token, currentDate, currentDate)
-        : console.log("Error al actualizar o cancelaste el modal");
-    });
-  }
-  openModalDelete(envio: any) {
-    console.log(envio);
-    const param = {
-      id_envio: envio.id_envio,
-      token: this.token,
-    };
-    this.deleteEntrega(param);
-  }
-  deleteEntrega({ id_envio, token }) {
-    const modalRef = this.dialog.open(ModalDeleteEnvioComponent, {
-      data: {
-        id_envio,
-        token,
-      },
-      minWidth: "400px",
-      maxWidth: "800px",
-    });
-    modalRef.afterClosed().subscribe((result) => {
-      console.log("cerrando modal");
-      console.log(result);
-      const currentDate = this.convertNewDateToString();
-      result
-        ? this.getDataByDate(this.token, currentDate, currentDate)
-        : console.log("Error al actualizar o cancelaste el modal");
-    });
-  }
+  selectSpinner(value: string) { this.valorSpinner = value; }
+
   filterList() {
-    this.data = [];
-    for (let element of this.datos) {
-      ("use strict");
-      let varia: string = element[this.valorSpinner];
-      if (varia != null) {
-        if (varia.toLowerCase().includes(this.searchTxt.toLowerCase())) {
-          this.data.push(element);
-        }
-      }
+    if (this.valorSpinner && this.valorSpinner !== '') {
+      this.data = this.datos.filter((element: any) => {
+        const varia: string = element[this.valorSpinner];
+        return varia && varia.toLowerCase().includes(this.searchTxt.toLowerCase());
+      });
+    } else {
+      this.valorSpinner = '';
+      return false;
     }
   }
 
   searchByDate() {
-    const inputStart: string = this.dateStartInput.nativeElement.value;
-    const inputEnd: string = this.dateEndInput.nativeElement.value;
+    this.searchTxt = undefined;
+    const { value: inputStart } = this.dateStartInput.nativeElement;
+    const { value: inputEnd } = this.dateEndInput.nativeElement;
 
-    this.validateNavigator(inputStart, inputEnd);
+    const currentDate = this.getCurrentDate();
+    const formattedStart = inputStart ? this.formatApíDate('string', inputStart) : currentDate;
+    const formattedEnd = inputEnd ? this.formatApíDate('string', inputEnd) : currentDate;
+
+    this.getDataByDate(this.token, formattedStart, formattedEnd);
   }
 
-  validateNavigator(dateStart: string, dateEnd: string) {
-    const navigatorVendor = window.navigator.vendor;
-    let start = "";
-    let end = "";
-    if (navigatorVendor.toLowerCase().includes("apple")) {
-      start = this.convertDateToString(dateStart, "apple");
-      end = this.convertDateToString(dateEnd, "apple");
-    } else {
-      start = this.convertDateToString(dateStart, "google");
-      end = this.convertDateToString(dateEnd, "google");
-    }
-    this.getDataByDate(this.token, start, end);
-  }
+  getDataByDate(token: string, dateStart: string, dateEnd: string) {
+    const urlApi = `${environment.url_api}/empresa/enviosFechas`;
 
-  convertNewDateToString() {
-    let currentDate = new Date().toLocaleDateString();
-    let [date, month, year] = currentDate.split("/");
-    let trasnformDate = this.trasnformDate(date);
-    let trasnforMonth = this.trasnformDate(month);
-    return `${trasnformDate}-${trasnforMonth}-${year}`;
-  }
-
-  trasnformDate(date: string) {
-    return date.length === 1 ? `0${date}` : date;
-  }
-
-  convertDateToString(date: string, sys: string) {
-    if (sys.toLowerCase() === "apple") {
-      return date.replace(/[/]/g, "-");
-    } else {
-      return `${date.split("-")[2]}-${date.split("-")[1]}-${date.split("-")[0]}`;
-    }
-  }
-
-  getDataByDate(token: string, date1: string, date2: string) {
-    console.log({ token: token, fecha1: date1, fecha2: date2 });
     this.spinnerService.show();
-    this.http
-      .post(
-        `${environment.url_api}/empresa/enviosFechas`,
-        // "https://backend-japi.herokuapp.com/empresa/enviosFechas",
-        { token: token, fecha1: date1, fecha2: date2 },
-        {
-          headers: {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Methods": "POST",
-            "Access-Control-Allow-Credentials": "true",
-            "Access-Control-Allow-Origin": "http://localhost:4200/*",
-          },
-        }
-      )
-      .subscribe((result: any) => {
-        console.log(result);
-        if (result.success) {
 
+    const headers = {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Methods": "POST",
+      "Access-Control-Allow-Credentials": "true",
+      "Access-Control-Allow-Origin": "*",
+    };
+
+    const requestData = {
+      token,
+      fecha1: dateStart,
+      fecha2: dateEnd
+    };
+
+    this.http.post(urlApi, requestData, { headers }).subscribe(
+      (result: any) => {
+        if (result.success) {
           const dataFormat = result.data.map((distritoData) => {
-            let { name } = this.distritos.find(
+            const { name } = this.distritos.find(
               (distrito) => distritoData.distrito_entrega === distrito.value
             );
-            var fondo: number;
-            let { estado } = this.estadoEntrega.find(
+
+            let fondo: number;
+            const { estado } = this.estadoEntrega.find(
               (estado) => estado.value === distritoData.estado
             );
-            if (estado == 'entregado') {
+
+            if (estado === 'entregado') {
               fondo = 1;
-            } else {
-              if (estado == 'caída') {
-                fondo = 2;
-              }
+            } else if (estado === 'caída') {
+              fondo = 2;
             }
+
             return {
               ...distritoData,
               nombre_estado: estado,
               distritoName: name,
-            };
-
+            };  
           });
 
-          console.log(dataFormat);
           this.data = dataFormat;
           this.datos = dataFormat;
         } else {
           console.log("error");
         }
         this.spinnerService.hide();
-      });
+      }, (error: any) => {
+        console.log(error);
+        this.spinnerService.hide();
+      }
+    );
   }
+
+  // editarEnvio(envio: any) {
+  //   console.log(envio);
+  //   const param = {
+  //     envio,
+  //     token: this.token,
+  //     distritos: this.distritos,
+  //     zonas: this.zonas,
+  //   };
+  //   this.ModalEditEnvios(param);
+  // }
+
+  // ModalEditEnvios({ envio, token, distritos, zonas }) {
+  //   const validate = 0;
+  //   const modalRef = this.dialog.open(ModalEnviosFechaComponent, {
+  //     data: {
+  //       envio,
+  //       token,
+  //       validate,
+  //       distritos,
+  //       zonas,
+  //     },
+  //     minWidth: "400px",
+  //     maxWidth: "800px",
+  //   });
+  //   modalRef.afterClosed().subscribe((result) => {
+  //     console.log("cerrando modal");
+  //     console.log(result);
+  //     const currentDate = this.convertNewDateToString();
+  //     result
+  //       ? this.getDataByDate(this.token, currentDate, currentDate)
+  //       : console.log("Error al actualizar o cancelaste el modal");
+  //   });
+  // }
+  // openModalDelete(envio: any) {
+  //   console.log(envio);
+  //   const param = {
+  //     id_envio: envio.id_envio,
+  //     token: this.token,
+  //   };
+  //   this.deleteEntrega(param);
+  // }
+  // deleteEntrega({ id_envio, token }) {
+  //   const modalRef = this.dialog.open(ModalDeleteEnvioComponent, {
+  //     data: {
+  //       id_envio,
+  //       token,
+  //     },
+  //     minWidth: "400px",
+  //     maxWidth: "800px",
+  //   });
+  //   modalRef.afterClosed().subscribe((result) => {
+  //     console.log("cerrando modal");
+  //     console.log(result);
+  //     const currentDate = this.convertNewDateToString();
+  //     result
+  //       ? this.getDataByDate(this.token, currentDate, currentDate)
+  //       : console.log("Error al actualizar o cancelaste el modal");
+  //   });
+  // }
 }
